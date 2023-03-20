36a672ce868ce293e302bc8f308e52f07891568a
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-03-20 10:29:37 -0500

    Navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomersComponent } from './components/customers/customers.component';
+import { SendMessageComponent } from './components/send-message/send-message.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"customers", component: CustomersComponent},
+  {path:"crm", component: SendMessageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { SendMessageComponent } from './components/send-message/send-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    SendMessageComponent
   ],
   imports: [
     BrowserModule,
