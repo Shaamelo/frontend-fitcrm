9d4e7bb4fe869421974cf639deb8029a08776211
GabrielCornejoB <gabrielcorbot@gmail.com>
2023-03-20 12:20:22 -0500

    UI Enviar mensaje

--- src/app/components/send-message/send-message.component.ts
+++ src/app/components/send-message/send-message.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CustomerService } from 'src/app/services/customer.service';
+import { Customer } from 'src/app/types/Customer';
 
 @Component({
   selector: 'app-send-message',
   templateUrl: './send-message.component.html',
   styleUrls: ['./send-message.component.sass']
 })
-export class SendMessageComponent {
-
+export class SendMessageComponent implements OnInit {
+  form: FormGroup;
+  customers: Customer[] = [];
+  constructor(private fb: FormBuilder, private customersService: CustomerService) {
+    this.form = this.fb.group({
+      type: ['', Validators.required],
+      target: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+  ngOnInit(): void {
+    this.customers = this.customersService.getCustomers();
+  }
+  sendMessage() {
+    alert("Mensaje enviado con exito");
+    this.form.reset();
+  }
 }
